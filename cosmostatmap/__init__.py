d6af47aaa84964e9ecd137f062caac750d2b4d7c
Samuel Farrens <samuel.farrens@gmail.com>
2020-03-31 13:24:24 +0200

    first commit

---
+++ cosmostatmap/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""COSMOSTATMAP PACKAGE
+
+The CosmoStatMap shows the diversity of the CosmoStat team.
+
+:Author: Samuel Farrens <samuel.farrens@cea.fr>
+
+"""
+
+from .info import __version__, __about__
